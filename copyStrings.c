fa41e3f6bf270245ad887a279d06a8193db702c0
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-08-19 10:37:21 -0700

    creates a function for copying strings

---
+++ copyStrings.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+
+
+/*copyStr: copies one string to another string
+
+ *@destination:string which is copied to
+
+ *@source:String copies from 
+
+ *Return: pointer to the destination string
+
+ */
+
+
+
+char *copyStr(char* restrict  destination, char* restrict source)
+
+{
+
+        int a = 0;
+
+        for (; source[a] != '\0'; a++)
+
+                destination[a] = source [a];
+
+        destination[a] = '\0';
+
+
+
+
+
+        return (destination);
+
+}
+
+
